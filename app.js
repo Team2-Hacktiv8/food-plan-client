f92843776a291ee6679555a7b4b9a4e09fad72ad
ryanadhi <ryanadhi.p@gmail.com>
2020-03-06 02:03:17 +0700

    LAYOUTING
    Neecesarry sections have been added

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 'use strict'
+
+$(document).ready (function(){
+
+    const token = localStorage.getItem('token') ;
+
+    if (token) {
+        $('#section-message').empty() ;
+        // showTodos(false)
+        $("#page-home").hide();
+        $("#page-dashboard").show();
+        $("#page-createplan").hide();
+        $("#page-updateplan").hide();
+    } else {
+        $('#section-message').empty() ;
+        $('#section-list').hide() ;
+        $("#page-home").show();
+        $("#page-dashboard").hide();
+        $("#page-createplan").hide();
+        $("#page-updateplan").hide();
+    }    
+})
