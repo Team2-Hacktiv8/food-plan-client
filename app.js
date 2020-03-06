43ea3dacb76025d5c98a7717474a8596a34a1c03
ryanadhi <ryanadhi.p@gmail.com>
2020-03-06 08:00:07 +0700

    Add login and registration form

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 'use strict'
+// BUTTON - PAGE
+function toLoginPage () {
+    $('#section-message').empty() ;
+    $("#page-home").hide();
+    $("#page-dashboard").hide();
+    $("#page-createplan").hide();
+    $("#page-updateplan").hide();
+    $("#page-login").show();
+    $("#page-register").hide();
+    $("#btn-login").hide();
+    $("#btn-register").hide();
+    $("#btn-logout").hide();
+}
 
-$(document).ready (function(){
+function toRegistrationPage () {
+    $('#section-message').empty() ;
+    $("#page-home").hide();
+    $("#page-dashboard").hide();
+    $("#page-createplan").hide();
+    $("#page-updateplan").hide();
+    $("#page-login").hide();
+    $("#page-register").show();
+    $("#btn-login").hide();
+    $("#btn-register").hide();
+    $("#btn-logout").hide();
+}
 
+function toHomePage() {
     const token = localStorage.getItem('token') ;
 
     if (token) {
         $('#section-message').empty() ;
-        // showTodos(false)
         $("#page-home").hide();
         $("#page-dashboard").show();
         $("#page-createplan").hide();
         $("#page-updateplan").hide();
+        $("#page-login").hide();
+        $("#page-register").hide();
+        $("#btn-login").hide();
+        $("#btn-register").hide();
+        $("#btn-logout").show();
     } else {
         $('#section-message').empty() ;
         $('#section-list').hide() ;
@@ -18,5 +47,119 @@ $(document).ready (function(){
         $("#page-dashboard").hide();
         $("#page-createplan").hide();
         $("#page-updateplan").hide();
+        $("#page-login").hide();
+        $("#page-register").hide();
+        $("#btn-login").show();
+        $("#btn-register").show();
+        $("#btn-logout").hide();
+    }
+}
+
+
+
+// SUBMISSION
+
+function register(event) {
+    event.preventDefault() ;
+    const email = $('#email-register').val();
+    const password = $('#password-register').val() ;
+
+    console.log(email , password);
+
+    // $('#email-register').val('');
+    // $('#password-register').val('')
+
+    // $.ajax({
+    //     method : 'POST',
+    //     url : 'http://localhost:3000/users/register',
+    //     data : {
+    //         email,
+    //         password
+    //     }
+    // })
+    //     .done(function (response) {
+    //         showMessage(['Signed up success'])
+    //         $('#section-message').empty() ;
+    //         $("#page-home").hide();
+    //         $("#page-dashboard").hide();
+    //         $("#page-createplan").hide();
+    //         $("#page-updateplan").hide();
+    //         $("#page-login").show();
+    //         $("#page-register").hide();
+    //         $("#btn-login").hide();
+    //         $("#btn-register").hide();
+    //         $("#btn-logout").hide();
+    //     })
+    //     .fail(function (err) {
+    //         showMessage(err.responseJSON.message)
+    //     })
+}
+
+function login(event) {
+    event.preventDefault() ;
+    const email = $('#email-login').val();
+    const password = $('#password-login').val() ;
+
+    console.log(email , password);
+
+    // $('#email-login').val('');
+    // $('#password-login').val('')
+
+    // $.ajax({
+    //     method : 'POST',
+    //     url : 'http://localhost:3000/users/login',
+    //     data : {
+    //         email,
+    //         password
+    //     }
+    // })
+    //     .done(function (response) {
+    //         showMessage(['Signed up success'])
+    //         $('#section-message').empty() ;
+    //         $("#page-home").hide();
+    //         $("#page-dashboard").show();
+    //         $("#page-createplan").hide();
+    //         $("#page-updateplan").hide();
+    //         $("#page-login").hide();
+    //         $("#page-register").hide();
+    //         $("#btn-login").hide();
+    //         $("#btn-register").hide();
+    //         $("#btn-logout").show();
+    //     })
+    //     .fail(function (err) {
+    //         showMessage(err.responseJSON.message)
+    //     })
+}
+
+
+
+
+$(document).ready (function(){
+
+    const token = localStorage.getItem('token') ;
+
+    if (token) {
+        $('#section-message').empty() ;
+        $("#page-home").hide();
+        $("#page-dashboard").show();
+        $("#page-createplan").hide();
+        $("#page-updateplan").hide();
+        $("#page-login").hide();
+        $("#page-register").hide();
+        $("#btn-login").hide();
+        $("#btn-register").hide();
+        $("#btn-logout").show();
+    } else {
+        $('#section-message').empty() ;
+        $('#section-list').hide() ;
+        $("#page-home").show();
+        $("#page-dashboard").hide();
+        $("#page-createplan").hide();
+        $("#page-updateplan").hide();
+        $("#page-login").hide();
+        $("#page-register").hide();
+        $("#btn-login").show();
+        $("#btn-register").show();
+        $("#btn-logout").hide();
     }    
 })
