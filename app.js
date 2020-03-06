6e41382cd11ffe1e01cda066b4f8d460faba4946
ditobprasetio <ditobagus28@gmail.com>
2020-03-06 14:46:48 +0700

    GOOGLE SignIn

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 'use strict'
+/* Login Google */
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    $.ajax({
+        method: "GET",
+        url: "http://localhost:3000/glogin",
+        headers: {
+            token: id_token
+        }
+    })
+    .done((token) => {
+        localStorage.setItem('token', token)
+        // fetchCookingPlans()
+        // tinggal tambahain hide dan show
+    })
+    .fail((err) => {
+        console.log(err);
+    })
+}
 
 /* Default View */
 
